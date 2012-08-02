3189bb046d1d3d224ecf1d3b95d9d33185370e72
John Napiorkowski <jjnapiork@cpan.org>
2012-08-02 19:53:24 +0000

    Import of JJNAPIORK/DBIx-Class-Migration-0.026 from CPAN.
    gitpan-cpan-distribution: DBIx-Class-Migration
    gitpan-cpan-version:      0.026
    gitpan-cpan-path:         JJNAPIORK/DBIx-Class-Migration-0.026.tar.gz
    gitpan-cpan-author:       JJNAPIORK
    gitpan-cpan-maturity:     released

--- lib/DBIx/Class/Migration.pm
+++ lib/DBIx/Class/Migration.pm
@@ -1,13 +1,13 @@
 package DBIx::Class::Migration;
 
-our $VERSION = "0.025";
+our $VERSION = "0.026";
 
 use Moose;
 use JSON::XS;
 use File::Copy 'cp';
 use File::Spec::Functions 'catdir', 'catfile';
 use File::Path 'mkpath', 'remove_tree';
-use MooseX::Types::LoadableClass 'LoadableClass';
+use DBIx::Class::Migration::Types 'LoadableClass', 'LoadableDBICSchemaClass';
 use Class::Load 'load_class';
 use Devel::PartialDump;
 use SQL::Translator;
@@ -42,7 +42,7 @@ has schema_class => (
   is => 'ro',
   predicate=>'has_schema_class',
   required=>0,
-  isa => LoadableClass,
+  isa => LoadableDBICSchemaClass,
   coerce=>1);
 
 has schema_args => (is=>'ro', isa=>'ArrayRef', lazy_build=>1);
